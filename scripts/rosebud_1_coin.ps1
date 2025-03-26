a26a40f9b09892c7a79e51fe0228b840160799a1
virginviolet <148799361+virginviolet@users.noreply.github.com>
2025-03-26 19:08:05 +0100

    Added pauses to rosebud scripts

--- scripts/rosebud_1_coin.ps1
+++ scripts/rosebud_1_coin.ps1
@@ -10,8 +10,10 @@ try {
         method = "manual"
     }
     Write-Host "Server URL: $Env:SERVER_URL"
+    Pause
     $body = @{"data" = @(@{"transaction" = $transaction})} | ConvertTo-Json -Depth 3
     Write-Host "Body: $body"
+    Pause
     Invoke-RestMethod -Uri "$Env:SERVER_URL/add_block" `
         -Method 'Post' `
         -Headers @{'token' = $Env:SERVER_TOKEN} `
